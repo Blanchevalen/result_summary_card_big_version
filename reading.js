67dd6ad9e026c72bd1c903f79c3324b46bdf8e31
HenriettTipi <122880245+Blanchevalen@users.noreply.github.com>
2023-10-08 20:04:30 +0200

    Update reading.js

--- reading.js
+++ reading.js
@@ -1,21 +1,15 @@
-const jsonURl='./data.json';
-fetch (jsonURl)
-    .then(response=>response.json())
-    .then(data=>{
+const jsonURL = './data.json';
+fetch(jsonURL)
+    .then(response => response.json())
+    .then(data => {
         console.log(data);
-        // JSON fájl betöltése és feldolgozása (például: data változóba)
-// ...
 
-// Az HTML elem kiválasztása, amelybe be szeretnénk tenni a score értékét
-//const scoreElement = document.getElementById('score');
-const scoreElement=document.getElementById('persPoint')
+        // Az HTML elem kiválasztása, amelybe be szeretnénk tenni a score értékét
+        const scoreElement = document.getElementById('persPoint');
 
-// Az első elem (index 0) score értékének lekérése
-//const scoreValue = data[0].score;
-const scoreValue=data[0].score;
+        // Az első elem (index 0) score értékének lekérése
+        const scoreValue = data[0].score;
 
-// A score értékének megjelenítése az HTML elemen
-//scoreElement.textContent = `Score: `;
-scoreElement.textcontent='${scoreValue}';
-
-    })
+        // A score értékének megjelenítése az HTML elemen
+        scoreElement.textContent = `Score: ${scoreValue}`;
+    });
