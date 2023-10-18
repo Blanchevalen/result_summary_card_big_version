526b4677b67c5a438ffdf0af26243129b25aa6c7
HenriettTipi <122880245+Blanchevalen@users.noreply.github.com>
2023-10-18 14:35:00 +0200

    Update reading.js

--- reading.js
+++ reading.js
@@ -6,10 +6,19 @@ fetch(jsonURL)
 
         // Az HTML elem kiválasztása, amelybe be szeretnénk tenni a score értékét
         const scoreElement = document.getElementById('persPoint');
+        const scoreElement1=document.getElementById('memPoint');
+        const scoreElement2=document.getElementById('verbPoint');
+        const scoreElement3=document.getElementById('visPoint');
 
         // Az első elem (index 0) score értékének lekérése
         const scoreValue = data[0].score;
+        const scoreValue1=data[1].score;
+        const scoreValue2=data[2].score;
+        const scoreValue3=data[3].score;
 
         // A score értékének megjelenítése az HTML elemen
         scoreElement.textContent = ` ${scoreValue} /100`;
+        scoreElement1.textContent = ` ${scoreValue1} /100`;
+        scoreElement2.textContent = ` ${scoreValue2} /100`;
+        scoreElement3.textContent = ` ${scoreValue3} /100`;
     });
